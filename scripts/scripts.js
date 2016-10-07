dc5c23563ecccca53328ea728e25f42e0c4177c5
amritrana <rana.amrit25@gmail.com>
2016-10-07 14:23:44 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,12 +3,12 @@ var hourNow = today.getHours();
 var greeting;
 
 if (hourNow > 18) {
-     greeting = ' Good Evening!';
-    } else if (hourNow > 12) {
+  greeting = ' Good Evening!';
+     } else if (hourNow > 12) {
   greeting = 'Good afternoon!';
-  } else if (hourNow > 0) {
-    greeting = 'Good morning!';
-  }else {
+     } else if (hourNow > 0) {
+  greeting = 'Good morning!';
+      }else {
     greeting = 'Welcome';
   }
   
